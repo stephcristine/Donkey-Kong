a1b5f4486bfe685ebb6f6b6ece9e28c470eab643
stephcristine <stephanecristine2@gmail.com>
2023-10-26 11:21:48 -0300

    Criação do primeiro mapa

--- banana.py
+++ banana.py
@@ -3,3 +3,15 @@ from caracteres import *
 class Banana(Caracteres):
     def __init__(self):
         pass
+
+b = '\033[0;33;40m█\033[m'
+a = '\033[0;33;40m▄\033[m'
+n = '\033[0;33;40m▀\033[m'
+
+print(" " + a)
+print(b)
+print(" " + n)
+
+t = '\033[1;33;40m(\033[m'
+
+print(t)
--- barrel.py
+++ barrel.py
@@ -1,5 +1,18 @@
-from caracteres import *
+#from caracteres import *
+#
+#class Berrel(Caracteres):
+#    def __init__(self):
+#        pass
 
-class Berrel(Caracteres):
-    def __init__(self):
-        pass
+l = '\033[0;34;40m█\033[m'
+t = '\033[0;33;40m█\033[m'
+i = '\033[0;31;40m█\033[m'
+       
+print("  "+l+l+l+l+l+l+l+l+l+l)                        
+print(" "+t+i+t+t+i+i+i+i+t+t+i+t)    
+print(t+i+i+t+i+i+i+i+i+i+t+i+i+t)                                    
+print(t+i+i+t+i+i+i+i+i+i+t+i+i+t)
+print(t+i+i+t+i+i+i+i+i+i+t+i+i+t)
+print(" "+t+i+t+t+i+i+i+i+t+t+i+t)    
+print("  "+l+l+l+l+l+l+l+l+l+l)
+
